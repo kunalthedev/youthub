c4792d0cd0a6851cb6f1561b757140a04b529399
Kunal Arora <10kunalji@gmail.com>
2021-07-06 23:03:04 +0530

    added boiler plate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Components/Header/Header';
+import Sidebar from './Components/Sidebar/Sidebar';
+import HomeScreen from './Screens/HomeScreen/HomeScreen';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className="app_container">
+        <Sidebar/>
+        <Container fluid className="app__main">
+          <HomeScreen />
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/Components/CategoriesBar/CategoriesBar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './_categoriesbar.scss';
+
+const CategoriesBar = () => {
+    return <div>CategoriesBar</div>;
+}
+
+export default CategoriesBar
