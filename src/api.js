c4691f7035e96bb1784336a788e740f905a414e0
Kunal Arora <10kunalji@gmail.com>
2021-07-16 11:29:48 +0530

    added api key in api

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
-    key: process.env.REACT_APP_YT_KEY,
+    key: "AIzaSyDkoU-RIcFE4PubYk_5j8onp8WGkk9ShSc",
   },
 });
 
